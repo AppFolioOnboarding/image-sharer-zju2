f323c168251d25947127fa64b3fd0f7f0de65e83
zhicheng ju <zhicheng.ju@gmail.com>
2019-10-27 13:48:35 -0700

    zj - Add Form to App

--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -4,20 +4,23 @@ import { describe, it } from 'mocha';
 
 import App from '../../components/App';
 import Header from '../../components/Header';
+import Form from '../../components/Form';
 import Footer from '../../components/Footer';
-import FeedbackStore from '../../stores/FeedbackStore';
 
 const assert = require('assert');
 
 describe('test the App', () => {
   it('App should have header and footer', () => {
-    const stores = {
-      feedbackStore: new FeedbackStore()
-    };
-    const app = shallow(<App stores={stores} />).dive();
+    const app = shallow(<App />);
     const header = app.find(Header);
     const footer = app.find(Footer);
     assert.strictEqual(header.props().title, 'Tell us what you think');
     assert.strictEqual(footer.props().content, 'Copyright: AppFolio Inc. Onboarding');
+  });
+
+  it('App should have a Form', () => {
+    const app = shallow(<App />);
+    const form = app.find(Form);
+    assert.strictEqual(form.length, 1);
   });
 });
