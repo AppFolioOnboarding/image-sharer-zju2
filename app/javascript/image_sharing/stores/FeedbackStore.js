2a9cb1e74597a78686541886a79ad65c1f23dae3
zhicheng ju <zhicheng.ju@gmail.com>
2019-10-27 12:46:32 -0700

    zj - Finish FeedbackStore

--- app/javascript/image_sharing/stores/FeedbackStore.js
+++ app/javascript/image_sharing/stores/FeedbackStore.js
@@ -1,5 +1,22 @@
+import { action, observable } from 'mobx';
+
 class FeedbackStore {
   /* Implement your feedback store*/
+  @observable name;
+  @observable comment;
+
+  constructor() {
+    this.name = '';
+    this.comment = '';
+  }
+
+  @action setName(name) {
+    this.name = name;
+  }
+
+  @action setComment(comment) {
+    this.comment = comment;
+  }
 }
 
 export default FeedbackStore;
